1e4727957141e49fcac5fcdcc7d935c1bb8d81e4
nguyendinhphong123 <deepsea221297@gmail.com>
2023-05-05 17:05:08 +0700

    test layout room

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoomController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
+});
+
+Route::prefix('rooms')->group(function () {
+    Route::get('/', [RoomController::class, 'index'])->name('room.index');
+    Route::get('/create', [RoomController::class, 'create'])->name('room.create');
+    Route::post('/store', [RoomController::class, 'store'])->name('room.store');
+    Route::get('/edit/{id}', [RoomController::class, 'edit'])->name('room.edit');
+    Route::put('/update/{id}', [RoomController::class, 'update'])->name('room.update');
+    Route::delete('/destroy/{id}', [RoomController::class, 'destroy'])->name('room.destroy');
+
 });
