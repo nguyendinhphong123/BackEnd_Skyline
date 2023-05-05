4eddee7cbeac076af5a415796982c45f55ee42e0
nguyendinhphong123 <deepsea221297@gmail.com>
2023-05-05 15:46:04 +0700

    complete feature-layout

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+@include('includes.header')
+@include('includes.sidebar')
+
+
+
+
+<div class="main-panel">
+	<div class="content-wrapper pb-0">
+		@yield('content')
+	</div>
+	@include('includes.footer')
+
+	<!-- main-panel ends -->
