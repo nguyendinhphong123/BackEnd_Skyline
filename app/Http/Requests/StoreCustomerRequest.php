bb5b9b0a321f6dc510d17b442ae7c72885b92458
nguyendinhphong123 <deepsea221297@gmail.com>
2023-05-18 11:23:29 +0700

    fix-validation

--- app/Http/Requests/StoreCustomerRequest.php
+++ app/Http/Requests/StoreCustomerRequest.php
@@ -22,7 +22,7 @@ class StoreCustomerRequest extends FormRequest
     public function rules(): array
     {
         return [
-                'name' => 'required',
+                'name' => 'required|unique',
                 'email' => 'required',
                 'password' => 'required',
                 'address' => 'required',
