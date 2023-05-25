bb6232340401a01ad7e137fe86a822feb08beb82
Huyentran226112 <huyentran221100@gmail.com>
2023-05-25 15:42:48 +0700

    feature_sendEmail

--- resources/views/emails/SendMail.blade.php
+++ resources/views/emails/SendMail.blade.php
@@ -5,9 +5,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+        }
+
+        .logo img {
+            width: 150px;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+            border-radius: 50%;
+        }
+
+        h1 {
+            font-size: 24px;
+            text-align: center;
+            margin-bottom: 20px;
+            color: #333;
+            text-transform: uppercase;
+        }
+
+        p {
+            font-size: 16px;
+            line-height: 1.5;
+            text-align: justify;
+            color: #555;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
+        <div class="logo">
+            <img src="https://huyentran226112.github.io/images/skyline.png" alt="Logo">
+        </div>
         {{--$datas = [
             'checkin' => $request->checkin,
             'checkout' => $request->checkout,
@@ -17,10 +57,19 @@
             'email' =>$user->email,
             'check' =>'confirmroom',
         ];  --}}
-    <h1>chao ban
+    <h1> Chào bạn :
         {{$data['user']->name}}
         </h1>
-       <p> cảm ơn vì đã đặt hàng .</p>
+        <p>
+        Kính gửi quý khách hàng,
+        Chúng tôi, đại gia đình khách sạn Skyline, xin chân thành cảm ơn sự tin tưởng và ủng hộ quý khách đã dành cho chúng tôi. Việc quý khách đã chọn Skyline để trải nghiệm những kỳ nghỉ và kinh nghiệm đáng nhớ là một vinh dự lớn đối với chúng tôi.
+        Chúng tôi cam kết luôn đặt sự hài lòng của quý khách lên hàng đầu và tạo ra môi trường lưu trú hoàn hảo. Đội ngũ nhân viên chuyên nghiệp và nhiệt tình của chúng tôi sẽ làm hết sức để đáp ứng mọi yêu cầu và mong muốn của quý khách trong suốt thời gian lưu trú tại khách sạn Skyline.
+        Chúng tôi hy vọng quý khách sẽ tận hưởng không chỉ những tiện nghi hiện đại và dịch vụ chất lượng mà chúng tôi cung cấp, mà còn cả không gian sang trọng và không khí ấm cúng của khách sạn Skyline.
+        Chúng tôi mong rằng trải nghiệm của quý khách sẽ vượt qua mong đợi và tạo ra những kỷ niệm đáng nhớ.
+        Một lần nữa, chân thành cảm ơn sự lựa chọn của quý khách. Chúng tôi mong muốn được đón tiếp quý khách một lần nữa tại khách sạn Skyline và mang đến cho quý khách những trải nghiệm tuyệt vời.
+        Trân trọng,
+        Đại gia đình khách sạn Skyline
+        </p>
     </div>
 </body>
 </html>
--- resources/views/emails/mail-notify.blade.php
+++ resources/views/emails/mail-notify.blade.php
@@ -72,20 +72,32 @@
             color: #007bff;
         }
 
-        .logo {
-            text-align: center;
-            margin-bottom: 20px;
+        .logo img {
+            width: 150px;
+            height: auto;
+            display: block;
+            margin: 0 auto;
+            border-radius: 50%;
         }
     </style>
 </head>
 <body>
     <div class="container">
         <div class="logo">
-            <img src="https://lh3.googleusercontent.com/-aVOCI1uHdknDwW5zPSWJtTNdAw5oT9wpcvH4YhZd-bGK3ALRqeDFeWrlPICH5eQAV4TMABEbOx16ox3A8zKRVuVw5PmTYLBoRMZO1XZ5kKWj6fju_nwfq1898FhbebwX3jAhCJnUTY8IrN3GrQr3K_PgCSQetBKJPxsk7C6KYo9y7cWH0-ph_PEUdp0zEXoE_l42kqos-WxYhGrRXr67QEs3I46fSrRDHL9tSRIq0t6v0NNa3loCqesgYM1YHws47Ms7oTLNmdLb87tXp2Q712spHmGruxphy45AwzYVNp8vDsD4qiFKgRZPczs0tROj8Dg_RmBlsXb1PnOUK-xzrbMcmxuuWKY-Ww1sWBM7HRncxdrDZiy_pduVV7rVBxNnLtUF8J_w8LsrGgBrOBeAQIKFJW-ZJ5hE9X-L0w_RI8oIN5uGkTOyfO7CVFrWZ3-H1RnJYBSlwn2rHjY6VXYfoidk7pP_kkkSSBCd_Q10nfJnQmMz2hindztjnDF0MBoBvgmrI68GN_YuDFCBLZDXryfMtcmLs-74kFfpGfDru1MWBAnvy2cS0nvEi5n_5-zW6EOtRBL7oavFtHQ-MIxS0Ni8eGA5D_dcHpasESBLfmTle6uvkW_0HL_LzM8lLkdIxicDxC-kosY5kZB1LRDNRbtlFDWhfML5tsteyCaD9rHq58sopkWEntjylCdl0K7dp0pjf8yAkxkgLNachpnJ9WmhF7dl30ffpP1BOW26m8VD5wqM6TS8lfMAwbI7FnjUuub7LLMHRwePOej36msp81xWVoLwiG1PUY6KNonsvXL51lAsaWCG_aAFa-0C9Yk-eRrNsPoHrGkPKVA0o1QeIwW8vACewBGosg17FCcb9DddaQ1K1r59xmfuGEDtG-S04L4SY4If0MStSW6Si-4D8sUmQAFnw3uzaWrvHPtJCc=w273-h119-s-no?authuser=0" alt="Logo">
+            <img src="https://huyentran226112.github.io/images/skyline.png" alt="Logo">
         </div>
-        <h2>Chào bạn: {{ $data['name'] }}</h2>
-        <h3>Chúng tôi đã khôi phục thành công tài khoản của bạn</h3>
-        <p>Mật khẩu của bạn là: <b>{{ $data['password'] }}</b></p>
+        <p>Chào bạn: {{ $data['name'] }}</p>
+        <p>Bạn đã yêu cầu cấp lại mật khẩu của mình. Chúng tôi đã nhận được yêu cầu này và đang tiến hành quy trình cấp lại mật khẩu cho bạn.
+            Dưới đây là thông tin cấp lại mật khẩu của bạn:  </p>
+           <p> Tên đăng nhập: {{ $data['email'] }}</p>
+           <p>   Mã đặc biệt: {{ $data['password'] }}</p>
+           <p> Xin cảm ơn bạn và chúc bạn một ngày tốt lành!
+            Trân trọng,
+            Bộ phận hỗ trợ khách hàng
+
+        </p>
     </div>
 </body>
 </html>
+
+
