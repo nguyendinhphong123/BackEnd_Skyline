97b3749b0c6fc71d236a2e735fb80384e2cfd319
Huyentran226112 <huyentran221100@gmail.com>
2023-05-25 08:34:44 +0700

    fix Ckeckout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -65,7 +65,7 @@ class AuthController extends Controller
             'password' => 'required||min:6',
             'phone' => 'required',
             'address' => 'required'
-           
+
         ]);
 
         if($validator->fails()){
