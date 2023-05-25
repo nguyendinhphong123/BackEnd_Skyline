4bae7c6f1e3f452814ee27cf3b8e0a7f4a05d146
Huyentran226112 <huyentran221100@gmail.com>
2023-05-25 08:50:53 +0700

    fix mail

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -3,36 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Jobs\SendEmail;
-use App\Models\Customer;
 use App\Models\Order;
-use App\Models\Room;
-use App\Models\User;
-use DateTime;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
     public function checkout(Request $request)
     {
-
         // $user = Auth::guard('customer')->user();
-            
+
         $data = $request->validate([
             'price' => 'required',
             'room_id' => 'required',
             'customer_id' => 'required',
             'checkin' => 'required',
             'checkout' => 'required',
-            
-           
-
-            // 'total' => 'required',
+            'total' => 'required',
         ]);
 
-       
-
         $order = Order::create($data);
+
         return response()->json([
             'success' => true,
             'order' => $order,
