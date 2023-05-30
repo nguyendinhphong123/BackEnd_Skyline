589fa40c515d6bf55f69ba6a1c64a8ede162e10d
nguyendinhphong123 <deepsea221297@gmail.com>
2023-05-30 08:34:21 +0700

    fix-layout-home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,6 @@
 <div class="page-header flex-wrap">
     <h3 class="mb-0"> Xin chào! <span class="pl-0 h6 pl-sm-2 text-muted d-inline-block">Chúc bạn có một ngày tốt lành.</span>
     </h3>
-    <div class="d-flex">
-        <button type="button" class="btn btn-sm bg-white btn-icon-text border">
-            <i class="mdi mdi-email btn-icon-prepend"></i> Email </button>
-        <button type="button" class="btn btn-sm bg-white btn-icon-text border ml-3">
-            <i class="mdi mdi-printer btn-icon-prepend"></i> Print </button>
-        <button type="button" class="btn btn-sm ml-3 btn-success"> Add User </button>
-    </div>
 </div>
 <div class="row">
     <div class="col-xl-3 col-lg-12 stretch-card grid-margin">
